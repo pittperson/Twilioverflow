ec3c847fef0632436e8a58b936725320c00b588e
Christopher Lies <clies@twilio.com>
2021-11-05 15:29:51 -0400

    added contact link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory, Redirect, Link } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { Container, Nav, Modal, Button } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
+import { Envelope } from "react-bootstrap-icons";
 import "./Header.scss";
 
 const Header = () => {
-  const history = useHistory();
+  const [show, setShow] = useState(false);
 
-  const clearFilters = () => {
-    return <Redirect to="/" />;
-  };
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <>
       <Navbar variant="dark" bg="danger" sticky="top">
         <Container>
-          <span style={{ fontWeight: "bold" }}>
-            <Navbar.Brand href="/">
-              <img
-                src="/logo512.png"
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-                alt="React Bootstrap logo"
-              />{" "}
-              Twilioverflow
-            </Navbar.Brand>
-          </span>
+          <Navbar.Brand href="/">
+            <img
+              src="/logo512.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />{" "}
+            Twilioverflow
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link onClick={handleShow}>About</Nav.Link>
+
+            <Nav.Link href="mailto:twilioverflow@twilioverflow.com">
+              Contact
+            </Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>About Twilioverflow</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            New to Twilio, I was browsing stack
+            <span style={{ fontWeight: "bold" }}>overflow</span> and me, being
+            me, thought, "there has to be a quicker way to consume everything
+            Twilio..." Enter Twilioverflow!
+          </div>
+          <br />
+          <div>
+            An aggregation of everything Twilio via stack
+            <span style={{ fontWeight: "bold" }}>overflow</span>. Want to drill
+            deeper? Just click on a tag below the title.
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
