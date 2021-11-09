b02c9a8ec3ab90cf7dad9b4227c358dd80230a0e
chris lies <chris@chrislies.com>
2021-11-09 10:00:08 -0500

    search stuff

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,18 +6,16 @@ import Container from "react-bootstrap/Container";
 const axios = require("axios");
 
 const Hero = (props) => {
-  // console.log(props);
-
   const cookies = new Cookies();
-  // console.log("Cookies: " + cookies.get("search"));
 
   const [titleList, setTitleList] = useState([]);
   const [nextPage, setNextPage] = useState(1);
   const [titleLimit, setTitleLimit] = useState(100);
   const [filters, setFilters] = useState("twilio;" + props?.filter ?? "");
+  const [searchFor, setSearchFor] = useState(cookies.get("search"));
 
   useEffect(() => {
-    getTitles(nextPage, titleLimit, filters);
+    // getTitles(nextPage, titleLimit, filters);
 
     return () => {};
   }, []);
@@ -30,7 +28,7 @@ const Hero = (props) => {
         document.body.clientHeight ===
         currentPosition + window.innerHeight - 10
       ) {
-        getTitles(nextPage, titleLimit, filters);
+        // getTitles(nextPage, titleLimit, filters);
       }
     }
 
@@ -38,62 +36,149 @@ const Hero = (props) => {
     return () => window.removeEventListener("scroll", onScroll);
   }, [nextPage]);
 
-  const getTitles = (pageNum, pageSize, nextPage) => {
-    axios
-      .get(
-        `https://api.stackexchange.com/2.3/questions?tagged=[${filters}]&page=${pageNum}&pagesize=${pageSize}&site=stackoverflow&key=DkLwlYTWw9AoNuzTYgmnUg((`
-      )
-      .then((res) => {
-        const titles = [];
-        const { items } = res.data;
+  // const getTitles = (pageNum, pageSize, nextPage) => {
+  //   axios
+  //     .get(
+  //       `https://api.stackexchange.com/2.3/questions?tagged=[${filters}]&page=${pageNum}&pagesize=${pageSize}&site=stackoverflow&key=DkLwlYTWw9AoNuzTYgmnUg((`
+  //     )
+  //     .then((res) => {
+  //       const titles = [];
+  //       const { items } = res.data;
 
-        let searchFor = cookies.get("search");
-        searchFor
-          ? console.log("Res Cookie: '" + searchFor + "'")
-          : (searchFor = "");
+  //       let searchFor = cookies.get("search");
+  //       searchFor
+  //         ? console.log("Res Cookie: '" + searchFor + "'")
+  //         : (searchFor = "");
 
-        searchFor.replace(/[+#]/g, "\\$&");
+  //       searchFor.replace(/[+#]/g, "\\$&");
 
-        if (searchFor) {
-          items.forEach((item) => {
-            if (item.title.match(searchFor)) {
-              titles.push(
-                <Post
-                  key={item.question_id}
-                  title={item.title}
-                  tags={item.tags}
-                  date={item.creation_date}
-                  link={item.link}
-                  answered={item.is_answered}
-                  filters={filters}
-                />
-              );
-            }
-          });
-          console.log(titles.length);
-        } else {
-          items.forEach((item) => {
-            titles.push(
-              <Post
-                key={item.question_id}
-                title={item.title}
-                tags={item.tags}
-                date={item.creation_date}
-                link={item.link}
-                answered={item.is_answered}
-                filters={filters}
-              />
-            );
-          });
-        }
+  //       if (searchFor) {
+  //         items.forEach((item) => {
+  //           if (item.title.match(searchFor)) {
+  //             titles.push(
+  //               <Post
+  //                 key={item.question_id}
+  //                 title={item.title}
+  //                 tags={item.tags}
+  //                 date={item.creation_date}
+  //                 link={item.link}
+  //                 answered={item.is_answered}
+  //                 filters={filters}
+  //               />
+  //             );
+  //           }
+  //         });
+  //         console.log(titles.length);
+  //       } else {
+  //         items.forEach((item) => {
+  //           titles.push(
+  //             <Post
+  //               key={item.question_id}
+  //               title={item.title}
+  //               tags={item.tags}
+  //               date={item.creation_date}
+  //               link={item.link}
+  //               answered={item.is_answered}
+  //               filters={filters}
+  //             />
+  //           );
+  //         });
+  //       }
 
-        setTitleList([...titleList, ...titles]); // Brad
-        setNextPage(pageNum + 1);
-      })
-      .catch((e) => {
-        console.log("error: ", e.message);
-      });
+  //       setTitleList([...titleList, ...titles]); // Brad
+  //       setNextPage(pageNum + 1);
+  //     })
+  //     .catch((e) => {
+  //       console.log("error: ", e.message);
+  //     });
+  // };
+
+  // ------------------
+
+  // const getTitles = async (pageNum, pageSize, nextPage) => {
+  //   axios
+  //     .get(
+  //       `https://api.stackexchange.com/2.3/questions?tagged=[${filters}]&page=${pageNum}&pagesize=${pageSize}&site=stackoverflow&key=DkLwlYTWw9AoNuzTYgmnUg((`
+  //     )
+  //     .then((res) => {
+  //       const titles = [];
+  //       const { items } = res.data;
+
+  //       // let searchFor = cookies.get("search");
+  //       // searchFor
+  //       //   ? console.log("Res Cookie: '" + searchFor + "'")
+  //       //   : (searchFor = "");
+
+  //       searchFor.replace(/[+#]/g, "\\$&");
+
+  //       if (searchFor) {
+  //         console.log("RegEx");
+  //         items.forEach((item) => {
+  //           if (item.title.match(searchFor)) {
+  //             titles.push(
+  //               <Post
+  //                 key={item.question_id}
+  //                 title={item.title}
+  //                 tags={item.tags}
+  //                 date={item.creation_date}
+  //                 link={item.link}
+  //                 answered={item.is_answered}
+  //                 filters={filters}
+  //               />
+  //             );
+  //           }
+  //         });
+  //         console.log(titles.length);
+  //       } else {
+  //         console.log("No RegEx");
+  //         items.forEach((item) => {
+  //           titles.push(
+  //             <Post
+  //               key={item.question_id}
+  //               title={item.title}
+  //               tags={item.tags}
+  //               date={item.creation_date}
+  //               link={item.link}
+  //               answered={item.is_answered}
+  //               filters={filters}
+  //             />
+  //           );
+  //         });
+  //       }
+
+  //       setTitleList([...titleList, ...titles]); // Brad
+  //       setNextPage(pageNum + 1);
+  //     })
+  //     .catch((e) => {
+  //       // console.log("error: ", e.message);
+  //     });
+
+  //   return titleList;
+  // };
+
+  const poo = async (page, articles) => {
+    let newArr = [];
+
+    for (let p = 1; p <= page; p++) {
+      for (let a = 1; a <= articles; a++) {
+        let page = p;
+        let article = a;
+        newArr.push(`Page: ${page}, Article: ${article}`);
+      }
+    }
+
+    return newArr;
   };
+
+  const collectAllSearchResults = async () => {
+    // for (let page = 1; page <= 10; page++) {
+    let result = await poo(10, 100);
+
+    console.log(result);
+    // }
+  };
+
+  collectAllSearchResults();
 
   return (
     <>
