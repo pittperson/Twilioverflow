679c9eb9c92f311fe66e91dd09a1c29af533348b
pittperson <chris@chrislies.com>
2021-11-18 17:01:22 -0500

    bar chart

--- src/components/Sankey/Sankey.js
+++ src/components/Sankey/Sankey.js
@@ -1,86 +1,69 @@
 import React, { useEffect, useState } from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import axios from "axios";
-import { getTwilioTags } from "../../helpers/getTwilioTags";
-import Chart from "react-google-charts";
-import ApexCharts from "apexcharts";
+import {
+  Chart,
+  Series,
+  CommonSeriesSettings,
+  Legend,
+  Export,
+  Tooltip,
+  Title,
+} from "devextreme-react/chart";
 
-const Sankey = (props) => {
-  const [tagRelationJson, setTagRelationJson] = useState([
-    ["From", "To", "Weight"],
-  ]);
-  const [twilioTags, setTwilioTags] = useState({});
+const Sankey = () => {
+  // const [twilioTags, setTwilioTags] = useState({});
+  const [twilioTags, setTwilioTags] = useState([]);
+  const [str1, setStr1] = useState("");
+  const [str2, setStr2] = useState("");
 
   useEffect(() => {
-    setTwilioTags({
-      series: [
-        { name: "a", data: [40] },
-        { name: "b", data: [30] },
-      ],
+    getTwilioTags("twilio").then((res1) => {
+      // console.log(
+      //   res1.data.items.forEach((item1) => {
+      //     console.log(item1);
+      //   })
+      // );
+      res1.data.items.forEach((item1) => {
+        getRelatedTags(item1.name).then((res2) => {
+          // console.log(`>>>>>>>>Item 1 Name: ${item1.name}`);
+          let tempTagRelationJson = [];
+          res2.data.items.forEach((item2) => {
+            if (item1.name !== item2.name) {
+              tempTagRelationJson.push({
+                key: item1.name,
+                name: item2.name,
+                data: item2.count,
+              });
+            }
+          });
+          setTwilioTags((prevState) => [...prevState, ...tempTagRelationJson]);
+        });
+      });
     });
-
-    console.log(`ddd: ${JSON.stringify(twilioTags)}`);
-
-    // setTwilioTags({ series: { ...twilioTags.series } });
-
-    // console.log(twilioTags);
-    // let poo = [];
-    // poo = {
-    //   series: [
-    //     { name: "a.poo", data: [40] },
-    //     { name: "b.poo", data: [30] },
-    //   ],
-    // };
-
-    // setTwilioTags({
-    //   ...twilioTags,
-    //   ...poo,
-    // });
-
-    // console.log({ ...twilioTags });
-
-    // console.log(...poo);
-
-    // setTwilioTags({
-    //   ...twilioTags,
-    //   ...twilioTags.series: series: {
-    //     name: "c",
-    //     data: [20],
-    //   },
-    // });
-    // item = "item1";
-    // setTwilioTags((prevState) => ({ ...prevState, item: "value1" }));
-    // console.log(tagRelationJson);
-    // getTwilioTags("twilio").then((res1) => {
-    //   // console.log(res1.data.items);
-    //   res1.data.items.forEach((item1) => {
-    //     getRelatedTags(item1.name).then((res2) => {
-    //       // console.log(`Item 1 Name: ${item1.name}`);
-    //       // console.log(res2.data.items);
-    //       let tempTagRelationJson = [];
-    //       res2.data.items.forEach((item2) => {
-    //         if (item1.name !== item2.name) {
-    //           // console.log(`${item1.name}, ${item2.name}, ${item2.count}`);
-    //           // tempTagRelationJson.push([item1.name, item2.name, item2.count]);
-    //           setTwilioTags((prevState) => ({
-    //             ...prevState,
-    //             series: [{ name: item1.name, data: [item2.count] }],
-    //           }));
-    //         }
-    //       });
-    //       // setTagRelationJson((oldArray) => [
-    //       //   ...oldArray,
-    //       //   ...tempTagRelationJson,
-    //       // ]);
-    //     });
-    //   });
-    // });
   }, []);
 
-  // console.log(twilioTags);
-
-  // console.log(tagRelationJson);
-  // console.log(JSON.stringify(tagRelationJson));
+  // let poop = [];
+  // poop = [
+  //   {
+  //     country: "USA",
+  //     hydro: 59.8,
+  //     oil: 937.6,
+  //     gas: 582,
+  //     coal: 564.3,
+  //     nuclear: 187.9,
+  //   },
+  //   {
+  //     country: "China",
+  //     hydro: 74.2,
+  //     oil: 308.6,
+  //     gas: 35.1,
+  //     coal: 956.9,
+  //     nuclear: 11.3,
+  //   },
+  // ];
+  // console.log(poop);
+  console.log(twilioTags);
 
   async function getTwilioTags(tagName) {
     let queryUrl = "";
@@ -99,7 +82,7 @@ const Sankey = (props) => {
   async function getRelatedTags(tagName) {
     let queryUrl = "";
 
-    queryUrl = `https://api.stackexchange.com/2.3/tags/${tagName}/related?pagesize=20&site=stackoverflow&key=DkLwlYTWw9AoNuzTYgmnUg((`;
+    queryUrl = `https://api.stackexchange.com/2.3/tags/${tagName}/related?pagesize=5&site=stackoverflow&key=DkLwlYTWw9AoNuzTYgmnUg((`;
     return axios
       .get(queryUrl)
       .then((res) => {
@@ -110,11 +93,41 @@ const Sankey = (props) => {
       });
   }
 
+  function customizeTooltip(arg) {
+    return {
+      text: `${arg.percentText} years: ${arg.valueText}`,
+    };
+  }
+
   return (
     <>
       <Container>
         <Row>
-          <Col></Col>
+          <Col>
+            <Chart id="chart" dataSource={twilioTags}>
+              <Title
+                text="Energy Consumption in 2004"
+                subtitle="(Millions of Tons, Oil Equivalent)"
+              />
+              <CommonSeriesSettings
+                argumentField="country"
+                type="fullstackedbar"
+              />
+              <Series valueField="hydro" name="Hydro-electric" />
+              <Series valueField="oil" name="Oil" />
+              <Series valueField="gas" name="Natural gas" />
+              <Series valueField="coal" name="Coal" />
+              <Series valueField="nuclear" name="Nuclear" />
+
+              <Legend
+                verticalAlignment="top"
+                horizontalAlignment="center"
+                itemTextPosition="right"
+              />
+              <Export enabled={true} />
+              <Tooltip enabled={true} customizeTooltip={customizeTooltip} />
+            </Chart>
+          </Col>
         </Row>
       </Container>
     </>
