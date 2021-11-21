fc8ed9960a6506f225a54dd8bcc0061595ce0a36
chris lies <chris@chrislies.com>
2021-11-21 17:54:53 -0500

    working charts

--- src/components/Sankey/Sankey.js
+++ src/components/Sankey/Sankey.js
@@ -1,100 +1,80 @@
 import React, { useEffect, useState } from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import axios from "axios";
-import { Bar } from 'react-chartjs-2';
-
-
-
-
+import { Bar } from "react-chartjs-2";
 
 const Sankey = () => {
-  // const [twilioTags, setTwilioTags] = useState({});
-  const [twilioTags, setTwilioTags] = useState([]);
-  const [keys, setKeys] = useState([]);
-  const [everything, setEverything] = useState({})
-
-  
+  const [dataArray, setDataArray] = useState({
+    labels: [],
+    datasets: [],
+  });
 
   useEffect(() => {
-    getTwilioTags("twilio").then((res1) => {
+    getTwilioTags("twilio").then(async (res1) => {
       let tempKeys = [];
-      res1.data.items.forEach((item1, index1) => {
-        if (tempKeys.indexOf(item1.name) < 0) {
-          tempKeys.push(item1.name);
+      let tempRelatedTags = [];
+      let tempDatasets = [];
+
+      for (let i = 0; i < res1.data.items.length; i++) {
+        let tempData = [];
+        let category = res1.data.items[i];
+        let catIndex = i;
+
+        if (tempKeys.indexOf(category.name) < 0) {
+          tempKeys.push(category.name);
+          tempData.push(0);
         }
 
-        getRelatedTags(item1.name).then((res2) => {
-          console.log(`<<<< ${item1.name} >>>>`)
-          let tempDatasets = [];
-          let tempData = [];
-          let tempEverything = [];
+        let relatedTagResponse = await getRelatedTags(category.name);
+        console.log(`<<<< ${category.name} >>>>`); // Tag from all tags with "twilio" in its name
 
-          res2.data.items.forEach((item2, index2) => {
-            if (item1.name !== item2.name) {
-              console.log(`${item2.name} : ${item2.count}`)
-              tempData.push(item2.count);
-            }
-          });
+        let tempEverything = [];
 
-          res2.data.items.forEach((item2, index2) => {
-            if (item1.name !== item2.name) {
-              tempDatasets.push({label: item2.name, data: tempData});
-            }
-          });
-          setTwilioTags((prevState) => [...prevState, ...tempDatasets]);
-          // tempEverything.push({labels: tempKeys});
+        relatedTagResponse.data.items.forEach((item2) => {
+          if (!tempRelatedTags.includes(item2.name)) {
+            tempRelatedTags.push(item2.name);
+            tempData[catIndex] = item2.count;
+
+            tempDatasets.push({
+              label: item2.name,
+              data: tempData,
+              backgroundColor: "rgb(54, 162, 235)",
+            });
+            tempData = [
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0,
+            ];
+          }
         });
+
+        tempEverything.push({ labels: tempKeys });
+      }
+
+      setDataArray({
+        labels: tempKeys,
+        datasets: tempDatasets,
       });
-      setKeys(tempKeys);
     });
   }, []);
-
-  console.log(keys);
-  console.log(twilioTags)
-  const all = ({labels: keys, datasets: twilioTags})
-  console.log(all)
-
-  const data = {
-    labels: ['1', '2', '3', '4', '5', '6'],
-    datasets: [
-      {
-        label: '# of Red Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: 'rgb(255, 99, 132)',
-      },
-      {
-        label: '# of Blue Votes',
-        data: [2, 3, 20, 5, 1, 4],
-        backgroundColor: 'rgb(54, 162, 235)',
-      },
-      {
-        label: '# of Green Votes',
-        data: [3, 10, 13, 15, 22, 30],
-        backgroundColor: 'rgb(75, 192, 192)',
-      },
-    ],
-  };
-
-  console.log(data)
 
   const options = {
     scales: {
       y: {
         stacked: true,
         ticks: {
-          beginAtZero: true
-        }
+          beginAtZero: false,
+        },
       },
       x: {
-        stacked: true
-      }
-    }
+        stacked: true,
+      },
+    },
   };
 
   async function getTwilioTags(tagName) {
     let queryUrl = "";
 
-    queryUrl = `https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&inname=${tagName}&pagesize=2&site=stackoverflow&key=DkLwlYTWw9AoNuzTYgmnUg((`;
+    queryUrl = `https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&inname=${tagName}&pagesize=100&site=stackoverflow&key=DkLwlYTWw9AoNuzTYgmnUg((`;
     return axios
       .get(queryUrl)
       .then((res) => {
@@ -108,7 +88,7 @@ const Sankey = () => {
   async function getRelatedTags(tagName) {
     let queryUrl = "";
 
-    queryUrl = `https://api.stackexchange.com/2.3/tags/${tagName}/related?pagesize=3&site=stackoverflow&key=DkLwlYTWw9AoNuzTYgmnUg((`;
+    queryUrl = `https://api.stackexchange.com/2.3/tags/${tagName}/related?pagesize=5&site=stackoverflow&key=DkLwlYTWw9AoNuzTYgmnUg((`;
     return axios
       .get(queryUrl)
       .then((res) => {
@@ -124,8 +104,8 @@ const Sankey = () => {
       <Container>
         <Row>
           <Col>
-          <Bar data={data} options={options} />
-          <Bar data={all} options={options} />
+            <Bar data={dataArray} options={options} redraw />
+            {/* <Bar data={data} options={options} /> */}
           </Col>
         </Row>
       </Container>
