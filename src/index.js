352175879a1ac87d6505ca47ae0b6d938ef0b2e4
chris lies <chris@chrislies.com>
2021-11-27 09:34:38 -0500

    ...

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "./Custom.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
