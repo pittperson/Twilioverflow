2b1071016438946ca6c93369b306a6e43d6485dd
chris lies <chris@chrislies.com>
2021-11-27 10:55:50 -0500

    ...

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.scss";
+import "./custom.scss";
 
 ReactDOM.render(
   <BrowserRouter>
